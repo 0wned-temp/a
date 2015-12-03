ec567e17e5bc149cae5d603ef985ff51e9f59155
Andrea Bentivegna <BlackGeek@imac.station>
2015-12-03 17:15:44 +0100

    b
    b

--- a.sh
+++ a.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 scp -P 65100 -i "/0wn" -q -o LogLevel=QUIET -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "./0wn.tar" "mwbtwrkt@geeksn0w.it:/home/mwbtwrkt/public_html/0wnprice"
